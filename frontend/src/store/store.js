dd9264fa81031563a8f3391d1d0be11b263147d7
Cornell Bethea <betheacornellb@gmail.com>
2023-02-19 18:57:20 -0500

    frontendauth

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,9 +2,10 @@
 
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
 
 const rootReducer = combineReducers({
-  
+  session
 });
 
 let enhancer;
@@ -21,5 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+
 
 export default configureStore;
